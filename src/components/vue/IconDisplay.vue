c7cc191ae8c00183d89834906d0e3a3448da5b57
Luifer1994 <almendralesluifer@gmail.com>
2024-07-16 11:17:05 -0500

    Fix display icons using vue 2

--- src/components/vue/IconDisplay.vue
+++ src/components/vue/IconDisplay.vue
@@ -1,13 +1,20 @@
 <template>
-  <div
-    class="text-white rounded w-12 h-12 flex items-center justify-center"
-    v-html="iconHTML"
-  />
+  <div class="text-white rounded w-12 h-12 flex items-center justify-center">
+    <img :src="iconSrc" alt="Icon" />
+  </div>
 </template>
+
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, computed } from 'vue';
 import { getIcon } from "../../utils/getIcon";
+
 const { title } = defineProps(['title']);
 const iconHTML = getIcon(title);
 
+const iconSrc = computed(() => {
+  if (iconHTML) {
+    return `data:image/svg+xml;utf8,${encodeURIComponent(iconHTML)}`;
+  }
+  return '';
+});
 </script>
