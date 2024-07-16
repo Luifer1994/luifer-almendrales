2968d737440324d9aebd3657177c309c769618d6
Luifer1994 <almendralesluifer@gmail.com>
2024-07-16 11:08:59 -0500

    Fix display icons using vue

---
+++ src/components/vue/IconDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="text-white rounded w-12 h-12 flex items-center justify-center"
+    v-html="iconHTML"
+  />
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { getIcon } from "../../utils/getIcon";
+const { title } = defineProps(['title']);
+const iconHTML = getIcon(title);
+
+</script>
