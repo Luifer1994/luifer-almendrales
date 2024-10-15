52d13bf60aca691ec4921ef8b1c3955f874910d1
Luifer1994 <almendralesluifer@gmail.com>
2024-10-15 11:20:46 -0500

    Refactor image source and update colors in tailwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,13 +5,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        dark: "#0d1117",  // Color de fondo general en modo oscuro
-        "dark-2": "#161b22",  // Color de fondo para elementos destacados en modo oscuro
-        "primary": "#0d6efd",  // Color primario para botones y enlaces destacados
-        "primary-hover": "#0b5ed7",  // Color de hover para el color primario
-        "text-light": "#c9d1d9",  // Color de texto en modo oscuro
-        "text-muted": "#8b949e",  // Color de texto para elementos menos destacados en modo oscuro
-		"shadow-dark": "#AAAAAA"
+        dark: "#0d1117", // Color de fondo general en modo oscuro
+        "dark-2": "#161b22", // Color de fondo para elementos destacados en modo oscuro
+        primary: "#0d6efd", // Color primario para botones y enlaces destacados
+        "primary-hover": "#0b5ed7", // Color de hover para el color primario
+        "text-light": "#c9d1d9", // Color de texto en modo oscuro
+        "text-muted": "#8b949e", // Color de texto para elementos menos destacados en modo oscuro
+        "shadow-dark": "#AAAAAA",
       },
     },
   },
